c4021bb471e0e893edaa412ee5680918dc47ff66
Nasir Nawaz <thebalosh69@gmail.com>
2019-05-06 13:56:39 +0500

    Rainbow.py
    A little Turtle Code, Which Create a rainbow and a colorful diagram.

---
+++ nd000 - Intro To Programing Nanodegree Program/nd000-04-Intro to Python, Part 1/nd000-04-Turtles and Code/Rainbow.py
@@ -0,0 +1,40 @@
+import turtle
+
+rainbow_color = ["red", "orange", "yellow", "green", "blue", "purple"]
+
+rainbow_iteration = [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 12, 13, 14, 15, 16, 17, 18, 19,]
+
+# Write whatever code you want here!
+# Import and Variable of RAINBOW
+rainbow = turtle.Turtle()
+rainbow.width(5)
+rainbow.penup()
+rainbow.back(160)
+rainbow.pendown()
+rainbow.left(70)
+
+# Rainbow CODE
+for color in rainbow_color:
+    for iteration in rainbow_iteration:
+        rainbow.right(7)
+        rainbow.forward(20)
+    rainbow.penup()
+    rainbow.right(118)
+    rainbow.forward(290)
+    rainbow.right(117)
+    rainbow.color(color)
+    rainbow.pendown()
+
+rainbow.penup()
+rainbow.right(153)
+rainbow.forward(40)
+rainbow.left(90)
+rainbow.forward(130)
+rainbow.pendown()
+
+for color in rainbow_color:
+    for i in [1, 2, 3, 4, 5]:
+        rainbow.forward(50)
+        rainbow.left(70)
+    rainbow.left(153)
+    rainbow.color(color)
